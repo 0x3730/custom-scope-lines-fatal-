b7dd541fe504802e2378d47996ad517658f004f4
Alexey Shilov <73469170+0x3730@users.noreply.github.com>
2021-06-28 04:58:59 +0300

    Update better scope lines.lua

--- better scope lines.lua
+++ better scope lines.lua
@@ -1,21 +1,37 @@
+local menu = fatality.menu
+local config = fatality.config
+local comboRemoveScope = menu:get_reference('visuals', 'misc', 'local', 'remove scope');
+
+local lines_enable = config:add_item("lines_enable", 1);
+local guiLinesCheckbox = menu:add_checkbox("Enable Custom Scope Lines", "visuals", "misc", "local", lines_enable);
+
+local margin_lines = config:add_item("margin_lines", 15);
+local guiMarginSlider = menu:add_slider("Scope Lines Margin", "visuals", "misc", "local", margin_lines, 0, 1000, 1);
+
+local size_lines = config:add_item("size_lines", 200);
+local guiSizeSlider = menu:add_slider("Scope Lines Size", "visuals", "misc", "local", size_lines, 0, 1000, 1);
+
+--
 local render = fatality.render
 local callbacks = fatality.callbacks;
 local ConVar = csgo.interface_handler:get_cvar();
 local screenCenter = render:screen_size();
 screenCenter.x = screenCenter.x * 0.5;
 screenCenter.y = screenCenter.y * 0.5;
+--
 
+local r, g, b = 194, 200, 255; -- change it if u wanna to change lines color
 
-local function on_paint()
+local function scopeline_handler()
 	if not lines_enable:get_bool() then
 		return;
 	end
 	
-	local localPlayer = csgo.interface_handler:get_entity_list():get_localplayer();
-	if not localPlayer:is_in_game() then
+	if not csgo.interface_handler:get_engine_client():is_in_game() then
 		return;
 	end
 	
+	local localPlayer = csgo.interface_handler:get_entity_list():get_localplayer();
 	local hudCvar = ConVar:find_var("cl_drawhud");
 	
 	if not localPlayer:is_alive() then
@@ -44,4 +60,4 @@ local function on_paint()
 	render:rect_fade(screenCenter.x, screenCenter.y + margin, 1, size, color, color2, false); -- down
 end
 
-callbacks:add("paint", on_paint);
+callbacks:add("paint", scopeline_handler);
